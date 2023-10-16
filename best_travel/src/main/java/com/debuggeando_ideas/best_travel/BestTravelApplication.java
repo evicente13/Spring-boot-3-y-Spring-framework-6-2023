bde8a12cb7cb11306994340c0785f3b36ddbe515
Eduardo Vicente <evicente.ti@gmail.com>
2023-10-15 22:19:07 -0500

    Video 27 - 15/10/2023 22:19

--- best_travel/src/main/java/com/debuggeando_ideas/best_travel/BestTravelApplication.java
+++ best_travel/src/main/java/com/debuggeando_ideas/best_travel/BestTravelApplication.java
@@ -1,13 +1,35 @@
 package com.debuggeando_ideas.best_travel;
 
+import com.debuggeando_ideas.best_travel.domain.repositories.FlyRepository;
+import com.debuggeando_ideas.best_travel.domain.repositories.HotelRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BestTravelApplication {
+@Slf4j
+public class BestTravelApplication implements CommandLineRunner {
+
+	@Autowired
+	private HotelRepository hotelRepository;
+
+	@Autowired
+	private FlyRepository flyRepository;
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BestTravelApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		var fly = flyRepository.findById(15L).get();
+		var hotel = hotelRepository.findById(7L).get();
+
+		log.info(String.valueOf(fly));
+		log.info(String.valueOf(hotel));
+	}
 }
